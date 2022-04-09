00d7b89ec3d44b946fd8dacac27ca9c04686771d
ImBilly <56002652+ImBilly01@users.noreply.github.com>
2022-04-09 14:08:05 +0200

    Add files via upload

---
+++ JavaScript Background Color Switcher/index.js
@@ -0,0 +1,48 @@
+// With Multiple Functions
+
+// function greyBackground() {
+//     document.body.style.backgroundColor = "grey";
+//     document.body.style.transition = "1s";
+// }
+
+// function whiteBackground() {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.transition = "1s";
+// }
+
+// function blueBackground() {
+//     document.body.style.backgroundColor = "blue";
+//     document.body.style.transition = "1s";
+// }
+
+// function yellowBackground() {
+//     document.body.style.backgroundColor = "yellow";
+//     document.body.style.transition = "1s";
+// }
+
+
+// With Event Listeners
+
+// let grey = document.getElementById('grey').addEventListener('click', () => {
+//     document.body.style.backgroundColor = "grey";
+// });
+
+// let white = document.getElementById('white').addEventListener('click', () => {
+//     document.body.style.backgroundColor = "white";
+// });;
+
+// let blue = document.getElementById('blue').addEventListener('click', () => {
+//     document.body.style.backgroundColor = "blue";
+// });;
+
+// let yellow = document.getElementById('yellow').addEventListener('click', () => {
+//     document.body.style.backgroundColor = "yellow";
+// });;
+
+
+// With 1 Function
+
+function changeBackground(color) {
+    document.body.style.background = color;
+    document.body.style.transition = '1s';
+}
